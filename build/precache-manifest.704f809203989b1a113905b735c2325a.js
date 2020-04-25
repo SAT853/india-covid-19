07866858de4f9fb6eb75ec170a4c7e4cca40c79b
SAT853 <sathishrpsg671@gmail.com>
2020-04-25 15:45:08 +0530

    gh-pages update 1.1

---
+++ build/precache-manifest.704f809203989b1a113905b735c2325a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be612eb03e0012e19d26a4999c86c70f",
+    "url": "/india-covid-19/index.html"
+  },
+  {
+    "revision": "a9281fb91eac6806e84d",
+    "url": "/india-covid-19/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "de4a81042d9984a89d56",
+    "url": "/india-covid-19/static/css/main.4aea6da3.chunk.css"
+  },
+  {
+    "revision": "a9281fb91eac6806e84d",
+    "url": "/india-covid-19/static/js/2.bd5a53df.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/india-covid-19/static/js/2.bd5a53df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de4a81042d9984a89d56",
+    "url": "/india-covid-19/static/js/main.901e92b1.chunk.js"
+  },
+  {
+    "revision": "451df62f52f0510085ea",
+    "url": "/india-covid-19/static/js/runtime-main.b9283583.js"
+  },
+  {
+    "revision": "a250df9d1bb0a3c20fe52676747174e3",
+    "url": "/india-covid-19/static/media/corona-logo.a250df9d.svg"
+  }
+]);
